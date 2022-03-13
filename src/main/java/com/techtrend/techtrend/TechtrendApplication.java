d804cf735a64a19ce5bf307951b802f188503ada
JhyunJung <jhyun.rigel@gmail.com>
2022-03-13 15:31:55 +0900

    project init

---
+++ src/main/java/com/techtrend/techtrend/TechtrendApplication.java
@@ -0,0 +1,13 @@
+package com.techtrend.techtrend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechtrendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechtrendApplication.class, args);
+	}
+
+}
